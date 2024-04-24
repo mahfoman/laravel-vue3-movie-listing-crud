85cbe7ea9ea4eb6abd160d162a1e12adc696e970
Md Mahfoozur Rahman <mahfoozur.rahman.bd@gmail.com>
2024-04-24 17:58:41 +0600

    movie create form with genre dropdown

--- resources/js/components/Movies/Create.vue
+++ resources/js/components/Movies/Create.vue
@@ -1,3 +1,38 @@
 <template>
-    <h1>Create Movie </h1>
+    <h3 class="mt-2">Create Movie </h3>
+    <div class="card mt-3">
+        <div class="card-body">
+            <form >
+                <div class="mb-3">
+                    <label for="email">Title:</label>
+                    <input type="text" class="form-control" id="text" placeholder="Enter title" name="title">
+                </div>
+                <div class="mb-3">
+                    <label for="email">Description:</label>
+                    <textarea  class="form-control" rows="5" id="description" name="description"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="pwd">Genre:</label>
+                    <select id="genre_id" class="form-control">
+                        <option value="" selected>-- Choose genre --</option>
+                        <option v-for="genre in genres" :value="genre.id">
+                            {{ genre.name }}
+                        </option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary cursor-not-allowed">
+                    <span>Save</span>
+                </button>
+            </form>
+        </div>
+    </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue';
+import useGenres from "../../composables/genres";
+const { genres, getGenres } = useGenres()
+onMounted(() => {
+    getGenres()
+})
+</script>
